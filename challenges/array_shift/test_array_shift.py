be8c58fd346ea75628e0a0be41bf494b38fc6774
Stephen <stephendeanharper@gmail.com>
2018-08-14 15:57:50 -0700

    added in tests and they pass. accounted for even and odd length

--- challenges/array_shift/test_array_shift.py
+++ challenges/array_shift/test_array_shift.py
@@ -1,3 +1,27 @@
-from .array_shift import array_shift
+from .array_shift import insertShiftArray
 import pytest
 
+
+def test_array_shift_module_exists():
+  assert insertShiftArray
+
+
+def test_list_gets_reversed_when_odd_number_length():
+  #arrange
+  expected = [1, 2, 3, 4]
+
+  #act
+  actual = insertShiftArray([1, 2, 4], 3)
+
+  #assert
+  assert expected == actual
+
+def test_list_gets_reversed_when_even_number_length():
+  #arrange
+  expected = [1, 2, 3, 4, 5]
+
+  #act
+  actual = insertShiftArray([1, 2, 4, 5], 3)
+
+  #assert
+  assert expected == actual
