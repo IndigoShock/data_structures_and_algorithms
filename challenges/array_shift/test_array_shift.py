fb922a4c056bc77b33b34ad279149b333dec5e97
Stephen <stephendeanharper@gmail.com>
2018-08-27 10:04:39 -0700

    merge ll image and the test array shift file

--- challenges/array_shift/test_array_shift.py
+++ challenges/array_shift/test_array_shift.py
@@ -3,18 +3,18 @@ import pytest
 
 
 def test_array_shift_module_exists():
-  assert insertShiftArray
+    assert insertShiftArray
 
 
 def test_list_gets_reversed_when_odd_number_length():
-  #arrange
-  expected = [1, 2, 3, 4]
+    # arrange
+    expected = [1, 2, 3, 4]
 
-  #act
-  actual = insertShiftArray([1, 2, 4], 3)
+    # act
+    actual = insertShiftArray([1, 2, 4], 3)
 
-  #assert
-  assert expected == actual
+    # assert
+    assert expected == actual
 
 
 def test_list_gets_reversed_when_even_number_length():
