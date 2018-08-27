99b8a2eb990acd8180658db5efcec6e36a42f5b8
Stephen <stephendeanharper@gmail.com>
2018-08-27 10:03:40 -0700

    updated docstrings and readme for linkedlists

--- data_structures/linked_list/node.py
+++ data_structures/linked_list/node.py
@@ -2,12 +2,22 @@ from typing import Any
 
 
 class Node(object):
+    """Node class which takes in an object with several methods below
+    """
     def __init__(self, val: Any, _next=None):
+        """The lines below are the value and reference or the val
+        and reference respectively.
+        """
         self.val = val
         self._next = _next
 
     def __str__(self):
+        """str shows the value
+        """
         return f'{self.val}'
 
     def __repr__(self):
+        """repr shows the value and the reference to the node
+        next to the current node.
+        """
         return f'<Node | Val: {self.val} | Next: {self._next}>'
