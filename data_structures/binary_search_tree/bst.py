4605bbc77460e7ea93784885b276440dad69bedf
Stephen <stephendeanharper@gmail.com>
2018-09-04 09:40:32 -0700

    working through bt in class

--- data_structures/binary_search_tree/bst.py
+++ data_structures/binary_search_tree/bst.py
@@ -1,6 +1,15 @@
+from .node import Node
+
+
 class BinaryTree:
     def __init__(self, iterable=None):
         self.root = None
+
+        if iterable is None:
+            iterable = []
+
+        for ele in iterable:
+            self.insert(ele)
 
     def __str__(self):
         """This shows the root node
@@ -12,7 +21,7 @@ class BinaryTree:
         """
         return f'Binary Tree. The root node is {self.root}'
 
-    def insert(self, callable=lambda node: print(node)):
+    def insert(self, val):
         """This will insert the wanted value into a binary tree.
         If the current node is not empty, the value will insert itself as the
         root. But if it is not empty, it will traverse through starting from
@@ -20,23 +29,26 @@ class BinaryTree:
         value. If so, assign the value as the left child. If greater than the
         current value, assign the value as right child.
         """
-        def _walk(root, new_node=None):
-            if root is not None:
-                root = new_node
 
-            if new_node.val <= root.val:
-                if root.left is not None:
-                    _walk(self.root.left, new_node)
+        node = Node(val)
+        if self.root is None:
+            self.root = node
 
-                else:
-                    root.left = new_node
+        current = self.root
+        while current:
+            if val <= current.val:
+                if current.left is None:
+                    current.left = node
+                    break
+                current = current.left
 
-            if new_node.val > root.val:
-                if root.right is not None:
-                    _walk(self.root.right, new_node)
+            if val > current.val:
+                if current.right is None:
+                    current.right = node
+                    break
+                current = current.right
 
-                else:
-                    root.right = new_node
+        return node
 
     def in_order(self, callable=lambda node: print(node)):
         """Go left, visit, then go right
--- data_structures/binary_search_tree/test_binary_search_tree.py
+++ data_structures/binary_search_tree/test_binary_search_tree.py
@@ -45,3 +45,35 @@ def test_insert_small_tree():
     bst = empty_tree()
     bst.insert(small_tree)
     assert bst is not None
+
+
+def test_empty_bt():
+    bt = BinaryTree()
+    assert isinstance(bt, BinaryTree)
+    assert bt.root is None
+
+
+def test_insert_into_empty_bt():
+    bt = BinaryTree()
+    assert bt.root is not None
+    bt.insert(25)
+    assert bt.root.value == 25
+
+
+def test_iterable_creates_bt():
+    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
+    assert bt.root.value == 20
+    assert bt.root.left.value == 18
+    assert bt.root.right.value == 40
+
+
+def test_inorder_traversal():
+    bt = BinaryTree([20, 18, 12, 19, 11, 14, 40, 31, 22, 33])
+    expected = [11, 12, 14, 18, 19, 20, 22, 31, 33, 40]
+    actual = []
+
+    def generate_list(node):
+        actual.append(node.value)
+
+    bt.in_order(generate_list)
+    assert expected == actual
